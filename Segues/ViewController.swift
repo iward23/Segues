ec68d540fafb4db897535660dd3f49b5d4177917
Imani Ward <idward712@gmail.com>
2022-10-27 14:32:49 -0400

    o

--- Segues/ViewController.swift
+++ Segues/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func unwindtoWhite(unwindSegue: UIStoryboardSegue){
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        segue.destination.navigationItem.title = textField.text
+    }
 
 }
 
